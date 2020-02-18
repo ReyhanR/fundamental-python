3119f25d172bb12702f9488b710e70961f80aa00
ReyhanR <rryanafi@gmail.com>
2020-02-18 17:16:57 +0700

    tambah jwbn hilman PhoneNum

--- Ujian Fundamental/PhoneNumber.py
+++ Ujian Fundamental/PhoneNumber.py
@@ -1,29 +1,49 @@
 
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-def phoneNumber(lisNum):
+# def phoneNumber(lisNum):
 
-    frontLis = []
-    CentLis = []
-    lastLis = []
+#     frontLis = []
+#     CentLis = []
+#     lastLis = []
 
-    while len(lisNum) == 9:
+#     while len(lisNum) == 9:
 
-        frontLis.insert(0, '(')
-        frontLis.insert(lisNum[0:3]
-    else:
+#         frontLis.insert(0, '(')
+#         frontLis.insert(lisNum[0:3]
+#     else:
 
-        return False
+#         return False
         
 
         
                 
     
 
-res = phoneNumber(number)
-print(res)
-                
+# res = phoneNumber(number)
+# print(res)
 
+##################                
+# jawaban hilman #
+##################
+
+def pnumber(x) :
+    res = len(x)
+    if res >9 :
+        phoneList = []
+        for num in x:
+            phoneList.append("". join(str(num)))
+        phStr = phoneList
+        print(phoneList)
+        ac = "".join(phStr[0:3])
+        prefix = "".join(phStr[3:6])
+        suffix = "".join(phStr[6:10]) 
+        print("({}) {}-{}".format(ac, prefix, suffix))
+    else :
+        print("False")
+    
+pnumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+pnumber([1, 2, 3, 4, 5, 6, 7, 8, 9])
             
 
 
